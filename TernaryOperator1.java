19c86784b544c5fa69f4c67e0cf32ba7646f0d1a
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-02-23 12:04:14 +0530

    Add files via upload

---
+++ TernaryOperator1.java
@@ -0,0 +1,11 @@
+class TernaryOperator1
+  {
+
+    public static void main(String[]args)
+    {
+      char a='A';
+      String result=(a%2==0)?("yes A is even"):("no A is not even");
+    
+      System.out.println(result);        //ASCII value of A is 65 and it is not divisible 2
+    }
+  }
